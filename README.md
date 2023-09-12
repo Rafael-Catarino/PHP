c4bd35d1d7299be57edf04449807c45ada169a36
Rafael Catarino <rafaelscatarino@hotmail.com>
2023-09-12 10:20:01 -0300

    adicionando o arquivo pegandoDadosDeUmForm

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PHP
+
+Neste repositório vai ser colocado toda a evolução do meu aprendizado com a linguagem PHP.
---
+++ pegandoDadosDeUmForm/recebendoDadosViaForm/configData.php
@@ -0,0 +1,5 @@
+<?php
+$formEmail = $_POST["email"];
+$formPassword = $_POST["password"];
+
+echo "email:" . $formEmail . "<br>senha:" . $formPassword;

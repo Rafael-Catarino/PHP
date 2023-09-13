d42c33fa517e1ab734a7e980f8e17ba4949f05d7
Rafael Catarino <rafaelscatarino@hotmail.com>
2023-09-12 22:33:14 -0300

    adicionando os aquivos comandoDeEscrita e constantes&Variaveis

---
+++ faculdade_cursos/01comandoDeEscrita&comentarios/index.php
@@ -0,0 +1,22 @@
+Antes
+<?php
+//comentario simples em PHP.
+
+/*
+Comentário de multiplas linhas em PHP.
+*/
+
+print("<hr>");
+print("Olá Mundo"); //Comando de escrita.
+print("<hr>");
+
+echo "Olá Mundo"; //Outro comando de escrita. 
+echo "<hr>";
+?>
+Durante
+<?php
+print("<hr>");
+echo "Aqui tem PHP.";
+echo "<hr>";
+?>
+Depois

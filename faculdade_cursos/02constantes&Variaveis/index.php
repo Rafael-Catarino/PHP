6a6ccf060b329c869931dd67bf660794a373b1f5
Rafael Catarino <rafaelscatarino@hotmail.com>
2023-09-15 14:11:59 -0300

    adicionando os arquivos while&DoWhile e for&ForEach

--- faculdade_cursos/02constantes&Variaveis/index.php
+++ faculdade_cursos/02constantes&Variaveis/index.php
@@ -1,17 +1,18 @@
 <?php
 //em php uma variável é declarada com o $ no começo.
-//EX:
-
 $numero1 = 10;
 $numero2 = 20;
 $texto = "Rafael";
 $bool = false;
 
+//Escrevendo os conteudos de uma variavel.
 echo "Numero1 - $numero1 <br> Numero2 - $numero2 <br> $numero1 + $numero2 = " . $numero1 + $numero2 . "<br> Texto: $texto <br> Bool: $bool";
 
 // constantes
 define("nome", "Rafael");
 define("pi", "3,14");
+
+//Escrevendo os conteudos das constantes.
 echo "<hr>";
 echo "Nome: " . nome . "<br>" . "PI: " . pi;
 
